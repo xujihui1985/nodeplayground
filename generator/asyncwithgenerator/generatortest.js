fb77a69e0e5467df183134794f118a6d8f60b856
Sean <xujihui1985@gmail.com>
2014-08-17 07:42:53 +0800

    add new example

---
+++ generator/asyncwithgenerator/generatortest.js
@@ -0,0 +1,29 @@
+function delay(done) {
+    setTimeout(function(){
+        done(null, 'delay done');
+    },1000);
+}
+
+var fs = require('fs');
+var stat = function(filename) {
+    return function (done) {
+        fs.stat(filename, done)
+    };
+};
+
+function *genFn() {
+    console.log('before delay');
+    console.log(yield stat('./generatortest.js'));
+    console.log(yield delay);
+    console.log('after delay');
+}
+
+var gen = genFn();
+next();
+
+function next (err, res) {
+    var ret = gen.next(res);
+    if (!ret.done) {
+        ret.value(next);
+    }
+}
